a776b84150899f8e843034f5a39f76aaa3d59b58
Wajahatulla Sharif <wajahathulla132@gmail.com>
2021-04-25 15:38:06 +0530

    Create Temperature.ino

---
+++ Temperature.ino
@@ -0,0 +1,49 @@
+//Temperature monitoring using Blynk IoT and 10k thermocouple
+
+#define BLYNK_PRINT DebugSerial
+
+
+// You could use a spare Hardware Serial on boards that have it (like Mega)
+#include <SoftwareSerial.h>
+SoftwareSerial DebugSerial(2, 3); // RX, TX
+
+#include <BlynkSimpleStream.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "Yf6liX_vltjD1qMs9FAYaJwmXL5wnYWL";
+
+const int thermistor_output = A1;
+
+void setup()
+{
+  // Debug console
+  DebugSerial.begin(9600);
+
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+}
+
+void loop()
+{
+  Blynk.run();
+  
+  int thermistor_adc_val;
+  double output_voltage, thermistor_resistance, therm_res_ln, temperature; 
+  
+  thermistor_adc_val = analogRead(thermistor_output);
+  output_voltage = ( (thermistor_adc_val * 5.0) / 1023.0 );
+  thermistor_resistance = ( ( 5 * ( 10.0 / output_voltage ) ) - 10 ); /* Resistance in kilo ohms */
+  thermistor_resistance = thermistor_resistance * 1000 ; /* Resistance in ohms   */
+  therm_res_ln = log(thermistor_resistance);
+  
+  /*  Steinhart-Hart Thermistor Equation: */
+  /*  Temperature in Kelvin = 1 / (A + B[ln(R)] + C[ln(R)]^3)   */
+  /*  where A = 0.001129148, B = 0.000234125 and C = 8.76741*10^-8  */
+  
+  temperature = ( 1 / ( 0.001129148 + ( 0.000234125 * therm_res_ln ) + ( 0.0000000876741 * therm_res_ln * therm_res_ln * therm_res_ln ) ) ); /* Temperature in Kelvin */
+  temperature = temperature - 273.15; /* Temperature in degree Celsius */
+  
+  Blynk.virtualWrite(10, temperature);
+  delay(1000);
+}
